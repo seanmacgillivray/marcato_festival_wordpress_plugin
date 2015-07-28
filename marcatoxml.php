ad40458393e8da2c85e770081f9a3c00155f003f
Adam Black <adamlb@gmail.com>
2015-07-28 12:22:22 -0300

    Check if custom fields are there before trying to use them

--- marcatoxml.php
+++ marcatoxml.php
@@ -731,8 +731,10 @@ class marcatoxml_importer {
 				foreach(array('name','bio_public','bio_limited','secondary_language_bio','homebase','web_photo_url','web_photo_url_root','photo_url','photo_url_root','updated_at') as $field){
 					$post_meta["marcato_artist_".$field] = nl2br((string)$artist->$field);
 				}
-				foreach($artist->{'custom-fields'}->{'custom-field'} as $field){
-					$post_meta["marcato_artist_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+				if(!empty($artist->{'custom-fields'})){
+					foreach($artist->{'custom-fields'}->{'custom-field'} as $field){
+						$post_meta["marcato_artist_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+					}
 				}
 				if(!empty($artist->shows)){
 					$i = 0;
@@ -836,8 +838,10 @@ class marcatoxml_importer {
 				foreach(array('name','description','street','city','province_state','country','postal_code','community','directions','longitude','latitude','primary_phone_number','photo_url','photo_url_root','updated_at') as $field){
 					$post_meta["marcato_venue_".$field] = $venue->$field;
 				}
-				foreach($venue->{'custom-fields'}->{'custom-field'} as $field){
-					$post_meta["marcato_venue_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+				if(!empty($venue->{'custom-fields'})){
+					foreach($venue->{'custom-fields'}->{'custom-field'} as $field){
+						$post_meta["marcato_venue_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+					}
 				}
 			}			
 			$posts[$index] = compact('post_content', 'post_title', 'post_type', 'post_marcato_id', 'post_status', 'post_meta','post_attachment', 'post_excerpt');
@@ -908,8 +912,10 @@ class marcatoxml_importer {
 				foreach(array('name','date','formatted_date','venue_name','formatted_start_time','start_time_unix','formatted_end_time','facebook_link','description_public','description_web','ticket_info','ticket_link','price','poster_url','poster_url_root','updated_at','seating') as $field){
 					$post_meta["marcato_show_".$field] = nl2br((string)$show->$field);
 				}
-				foreach($show->{'custom-fields'}->{'custom-field'} as $field){
-					$post_meta["marcato_show_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+				if(!empty($show->{'custom-fields'})){
+					foreach($show->{'custom-fields'}->{'custom-field'} as $field){
+						$post_meta["marcato_show_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+					}
 				}
 				foreach($show->venue as $venue){
 					foreach(array('name','street','city','province_state','community','longitute','latitude','id') as $field){
@@ -1017,8 +1023,10 @@ class marcatoxml_importer {
 				foreach(array('name','date','formatted_date','venue_name','formatted_start_time','start_time_unix','end_time','formatted_end_time','facebook_link','description_public','description_web','ticket_info','ticket_link','price','poster_url','poster_url_root','event_contact_summary','event_contact_name','event_contact_phone','event_contact_email','hosting_organization_title','updated_at','seating') as $field){
 					$post_meta["marcato_workshop_".$field] = nl2br((string)$workshop->$field);
 				}
-				foreach($workshop->{'custom-fields'}->{'custom-field'} as $field){
-					$post_meta["marcato_workshop_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+				if(!empty($workshop->{'custom-fields'})){
+					foreach($workshop->{'custom-fields'}->{'custom-field'} as $field){
+						$post_meta["marcato_workshop_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+					}
 				}
 				if(!empty($workshop->workshop_types)){
 					$i = 0;
@@ -1097,8 +1105,10 @@ class marcatoxml_importer {
 				foreach(array('bio','company','id','industry','name','position','updated_at','photo_url','photo_url_root','photo_fingerprint','email') as $field){
 					$post_meta["marcato_contact_".$field] = nl2br((string)$contact->$field);
 				}
-				foreach($contact->{'custom-fields'}->{'custom-field'} as $field){
-					$post_meta["marcato_contact_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+				if(!empty($contact->{'custom-fields'})){
+					foreach($contact->{'custom-fields'}->{'custom-field'} as $field){
+						$post_meta["marcato_contact_custom_field_".$field->{'form-section-name'}."_".$field->label] = nl2br((string)$field->value);
+					}
 				}
 			}			
 			$posts[$index] = compact('post_content', 'post_title', 'post_type', 'post_marcato_id','post_attachment','post_meta','post_excerpt');
